37ba6d5c23290759adb408a8f90b02c7b9b4033c
Joseph <geometryhuayra@gmail.com>
2024-02-05 17:35:37 -0500

    primer commit segundo intento

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#6F6F6F",
+      },
+    },
+  },
+  plugins: [],
+};
